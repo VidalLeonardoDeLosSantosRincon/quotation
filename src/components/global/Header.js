bbd293b5d3e45bc0c245c53f3a9bde48152bb7b0
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2019-10-01 11:49:19 -0400

    header was modified , the navmenu was created and added to the header and header's icon too

--- src/components/global/Header.js
+++ src/components/global/Header.js
@@ -1,6 +1,7 @@
 import React,{Component, Fragment} from "react";
 
 import "../../assets/css/Header.css"; 
+import ratesIcon from "../../assets/images/rates.png";
 
 class Header extends Component{
     render(){
@@ -8,7 +9,16 @@ class Header extends Component{
             <Fragment>
                 <div className="ctr-header">
                     <div className="header">
-                        asas
+                        <span id="logo-box">
+                            <img src={ratesIcon} alt="rates_logo"/>
+                        </span>
+                        <span id="menu-box">
+                            <ul id="menu">
+                                <a href="/"><li className="menu-item">Home</li></a>
+                                <a href="/rates"><li className="menu-item">Rates</li></a>
+                                <a href="/calculate"><li className="menu-item">Calculate</li></a>
+                            </ul>
+                        </span>
                     </div>
                 </div>
             </Fragment>
